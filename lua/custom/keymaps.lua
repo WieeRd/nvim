fa34f1a465b1bb8d1092b450660472d94ddc8571
WieeRd <>
2022-05-16 22:47:10 +0900

    Alt + hjkl window resizing now support count

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -12,6 +12,9 @@ map('i', "kj", "<Esc>")
 -- Clear search highlights when redrawing screen with Ctrl+L
 map('n', "<C-l>", "<Cmd>noh<CR><C-l>")
 
+-- Clear search highlights and last executed command with <Esc>
+map('n', "<Esc>", "<Esc><Cmd>noh <Bar> echo ''<CR>")
+
 -- I keep using Ctrl+S unconsciously even when using vim
 map('n', "<C-s>", "<Cmd>silent write<CR>")
 
@@ -19,29 +22,33 @@ map('n', "<C-s>", "<Cmd>silent write<CR>")
 map('n', "yp", [["0p]])  -- paste from yank register
 map('n', "yd", [["0d]])  -- delete into yank register
 
--- ALT + hjkl : Resize window
-map('n', "<M-j>", "<Cmd>resize -1<CR>")
-map('n', "<M-k>", "<Cmd>resize +1<CR>")
-map('n', "<M-h>", "<Cmd>vertical resize -1<CR>")
-map('n', "<M-l>", "<Cmd>vertical resize +1<CR>")
+-- Resize window with Alt + hjkl
+map({ 'n', 'o' }, "<M-j>", "<C-w>-")
+map({ 'n', 'o' }, "<M-k>", "<C-w>+")
+map({ 'n', 'o' }, "<M-h>", "<C-w><")
+map({ 'n', 'o' }, "<M-l>", "<C-w>>")
 
 -- Tab navigation
-map('n', "]t", "gt")
-map('n', "[t", "gT")
+map({ 'n', 'o' }, "]t", "gt")
+map({ 'n', 'o' }, "[t", "gT")
+
 map('n', "<Leader>1", "1gt")
 map('n', "<Leader>2", "2gt")
 map('n', "<Leader>3", "3gt")
 map('n', "<Leader>4", "4gt")
+
+map('n', "<Leader>+", "<Cmd>tabnew<CR>")
+map('n', "<Leader>-", "<Cmd>tabclose<CR>")
 
 -- Make mouse wheel scroll page instead of moving cursor
 map({ 'n', 'v' }, "<Up>", "<C-y>")
 map({ 'n', 'v' }, "<Down>", "<C-e>")
 
 -- TODO: Left/Right arrow keys
-map('n', "<Left>", "gT")
-map('n', "<Right>", "gt")
+-- map({ 'n', 'o' }, "<Left>", "gT")
+-- map({ 'n', 'o' }, "<Right>", "gt")
 
--- F1 ~ F12 : TODO
+-- TODO: F1 ~ F12
 -- map('n', "<F1>", "<Cmd>so Session.vim<CR><C-l>")    -- local session (.)
 -- map('n', "<F2>", "<Cmd>mks! Session.vim <Bar> wqa<CR>")
 -- map('n', "<F3>", "<Cmd>so ~/Session.vim<CR><C-l>")  -- global session (~)
