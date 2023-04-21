ae8263e90b6f6540264a722fcedea98d0b26ff15
WieeRd <wowluke@naver.com>
2023-04-22 01:07:22 +0900

    refactor: keymaps.lua is now a single table

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,70 +1,82 @@
 -- [[ keymaps: ':map' stuff ]] --
-local map = vim.keymap.set
 
 -- <Space> as a <Leader> key
 vim.g.mapleader = " "
-map({ "n", "x" }, " ", "")
 
--- Esc alternative
-map("i", "kj", "<Esc>")
+local mappings = {
+  -- insert
+  i = {
+    -- Esc alternative
+    ["kj"] = "<Esc>",
+  },
 
--- Clear search highlights and last executed command with <Esc>
-map("n", "<Esc>", "<Esc><Cmd>noh <Bar> echo ''<CR>")
+  -- normal
+  n = {
+    -- clear search highlights and last executed command with <Esc>
+    ["<Esc>"] = "<Esc><Cmd>noh <Bar> echo ''<CR>",
 
--- Frequently used commands
-map("n", "<Leader>w", "<Cmd>silent write<CR>")
-map("n", "<Leader>W", "<Cmd>silent wall<CR>")
-map("n", "<Leader>q", "<Cmd>quit<CR>")
-map("n", "<Leader>Q", "<Cmd>quit!<CR>")
+    -- frequently used commands
+    ["<Leader>w"] = "<Cmd>silent write<CR>",
+    ["<Leader>W"] = "<Cmd>silent wall<CR>",
+    ["<Leader>q"] = "<Cmd>quit<CR>",
+    ["<Leader>Q"] = "<Cmd>quit!<CR>",
 
--- Open cmdline window with `:lua =` prefilled
-map("n", "<Leader>=", ":lua =")
+    -- open cmdline window with `:lua =` prefilled
+    ["<Leader>="] = ":lua =",
 
--- I hate magic
-map("n", "/", "/\\V") -- very nomagic
-map("n", "?", "?\\V") -- very nomagic
+    -- I hate magic
+    -- ["/"] = { "/\\V" }, -- very nomagic
+    -- ["?"] = { "?\\V" }, -- very nomagic
 
--- I like very magic
-map("n", "g/", "/\\v") -- very magic
-map("n", "g?", "?\\v") -- very magic
+    -- I like very magic
+    ["g/"] = "/\\v", -- very magic
+    ["g?"] = "?\\v", -- very magic
 
--- Delete without worrying about yanked content
-map({ "n", "v" }, "yp", [["0p]]) -- paste from yank register
-map({ "n", "v" }, "yd", [["0d]]) -- delete into yank register
+    -- tab navigation
+    ["]t"] = "gt",
+    ["[t"] = "gT",
+    ["]T"] = "<Cmd>tabmove +1<CR>",
+    ["[T"] = "<Cmd>tabmove -1<CR>",
 
--- Windows navigation
--- map("n", "<Leader>w", "<C-w>", { remap = true })
+    ["<Leader>1"] = "1gt",
+    ["<Leader>2"] = "2gt",
+    ["<Leader>3"] = "3gt",
+    ["<Leader>4"] = "4gt",
 
--- Tab navigation
-map({ "n", "o" }, "]t", "gt")
-map({ "n", "o" }, "[t", "gT")
-map({ "n", "o" }, "<Right>", "gt")
-map({ "n", "o" }, "<Left>", "gT")
+    ["<Leader>+"] = "<Cmd>tabnew<CR>",
+    ["<Leader>-"] = "<Cmd>tabclose<CR>",
+    ["<Leader>0"] = "<Cmd>tabonly<CR>",
+  },
 
-map("n", "]T", "<Cmd>tabmove +1<CR>")
-map("n", "[T", "<Cmd>tabmove -1<CR>")
+  -- normal & visual
+  [{ "n", "v" }] = {
+    [" "] = "",
 
-map("n", "<Leader>1", "1gt")
-map("n", "<Leader>2", "2gt")
-map("n", "<Leader>3", "3gt")
-map("n", "<Leader>4", "4gt")
+    -- scroll page with mouse wheel
+    ["<Up>"] = "<C-y>",
+    ["<Down>"] = "<C-e>",
 
-map("n", "<Leader>+", "<Cmd>tabnew<CR>")
-map("n", "<Leader>-", "<Cmd>tabclose<CR>")
-map("n", "<Leader>0", "<Cmd>tabonly<CR>")
+    -- delete without worrying about yanked content
+    ["yp"] = [["0p]], -- paste from yank register
+    ["yd"] = [["0d]], -- delete into yank register
+  },
 
--- Make mouse wheel scroll page instead of moving cursor
-map({ "n", "v" }, "<Up>", "<C-y>")
-map({ "n", "v" }, "<Down>", "<C-e>")
+  -- text object
+  [{ "o", "v" }] = {
+    ["."] = "iw",
+    [","] = "aW",
+    ["ae"] = function()
+      vim.cmd("norm! m'vV")
+      vim.cmd("keepjumps 0")
+      vim.cmd("norm! o")
+      vim.cmd("keepjumps $")
+    end,
+  },
+}
 
--- 'entire buffer' text object
-map({ "o", "v" }, "ae", function()
-  vim.cmd("norm! m'vV")
-  vim.cmd("keepjumps 0")
-  vim.cmd("norm! o")
-  vim.cmd("keepjumps $")
-end)
-
--- `.` and `,` as word text objects
-map({ "o", "v" }, ".", "iw")
-map({ "o", "v" }, ",", "aW")
+local map = vim.keymap.set
+for mode, mapping in pairs(mappings) do
+  for lhs, rhs in pairs(mapping) do
+    map(mode, lhs, rhs)
+  end
+end
