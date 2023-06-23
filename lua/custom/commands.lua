208bee4bba7089d8f6ce597b8ad1645acbbddac9
WieeRd <wowluke@naver.com>
2023-06-23 15:28:44 +0900

    chore: remove legacy code

--- lua/custom/commands.lua
+++
@@ -1,8 +0,0 @@
-local command = vim.api.nvim_create_user_command
-
-command("Dump", function(opts)
-  vim.cmd(("put =execute('%s')"):format(opts.args))
-end, { nargs = 1 })
-
--- TODO: reload configs
--- TODO: autoformat autocmd/command
