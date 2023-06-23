93a5dc2fe16c4b11f745f75afc37065f4b5668b6
WieeRd <wowluke@naver.com>
2023-06-23 16:09:24 +0900

    feat(autocmd): go to last visited tab when current tab is closed

--- lua/builtin/autocmds.lua
+++ lua/builtin/autocmds.lua
@@ -33,11 +33,27 @@ return {
     desc = "briefly highlight the yanked text",
   },
 
-  -- FEAT: ASAP: smart float window background
   {
     "TabEnter",
     callback = function(_)
-      -- vim.g.last_accessed_tab = vim.api.nvim_get_current_tabpage()
-    end
-  }
+      local g = vim.g
+      g.tabpage_prev = g.tabpage_curr
+      g.tabpage_curr = vim.api.nvim_get_current_tabpage()
+    end,
+    desc = "remember last visited tabpage",
+  },
+
+  {
+    "TabClosed",
+    callback = function(_)
+      local g, api = vim.g, vim.api
+      if
+        g.tabpage_curr ~= api.nvim_get_current_tabpage()
+        and api.nvim_tabpage_is_valid(g.tabpage_prev)
+      then
+        api.nvim_set_current_tabpage(g.tabpage_prev)
+      end
+    end,
+    desc = "go to last visited tab when current tab is closed",
+  },
 }
