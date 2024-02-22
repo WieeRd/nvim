8417a3dbf9cd6db1f0f4f2fe9023deb724d94909
WieeRd <verywieerd@gmail.com>
2024-02-22 19:25:49 +0900

    chore: add `.todo.lua`

---
+++ .todo.lua
@@ -0,0 +1,69 @@
+-- NOTE: below are months to years old chaotic lists of TODOs,
+-- | with no consistency whatsoever and at this point I have no clue
+-- | what some of these ideas were meant to be.
+-- | I'm just gonna archive all of them before the great dotfiles migration
+
+
+-- NOTE: fluid simulation and sound
+-- TODO: codebreak.rs (TUI game)
+
+-- FIXME: add keymap descs + which-key groups
+-- FIXME: float border using blend
+-- FIXME: ltex_extra just got new setup method
+
+-- TODO: heirline dap components
+-- TODO: lsp codelens
+
+-- TODO: aerial filter based on parents type
+
+-- TODO: lspconfig should be server setup only, move keymaps to keymaps.lua
+-- NOTE: check out codicons
+
+-- lequire(foo).bar == function() require("foo").bar end
+
+-- TODO: cartography.nvim
+-- by default: lua port of vim-unimpaired
+-- toggle boolean options (true, false)
+-- rotate option values
+-- handy bracket mappings (pair of commands)
+--
+-- toggle options
+-- { 's' = "spell", 'n', "number" }
+-- rotate options
+-- { 'f', foldcolumn = { "0", "auto:9" } }
+-- { key = 'f', opt = "foldcolumn", values = { "0", "auto:9" } }
+-- run commands
+-- { 'i' = "IndentBlanklineToggle", 't' = { "TSBufEnable", "TSBufDisable" } }
+
+-- NOTE: keysounds.nvim
+
+-- NOTE: TS structure highlight
+-- - indentation guide color based on context type (func, class, struct)
+-- - highlight blocks using TS (better view of code structure)
+
+-- NOTE: lsp_lines - override open_float instead of vlines
+
+-- NOTE: glowberry.nvim
+-- LSP UI using Glow/Glamour (hover, signature, cmp item preview, etc)
+
+-- TODO: improve glow (PR, maintained fork, or rewrite in other lang)
+-- preserve reference links & convert links to reference links
+-- codeblock background highlight
+
+-- NOTE: colorwheel.nvim
+-- switch colorscheme styles
+-- `:Style` command (`let g:` + `:colo`)
+-- `prev_style()`, `next_style()` API
+-- `ColorSchemePre` autocmd (select next style when running on same colorscheme)
+-- Telescope / Trouble list
+
+-- NOTE: more organized, formatted vimdoc
+-- asciidoc, markdown, idk
+
+-- NOTE: BookTree
+-- branching story; 'episode' instead of 'commit'
+-- episode types: 'continued', 'spin-off'
+-- episode flags: 'epilogue', 'prologue'
+
+-- TODO: delete comment EOL keybind
+-- TODO: keep track of external dependencies
